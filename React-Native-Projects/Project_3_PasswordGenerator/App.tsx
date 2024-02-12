fabb3e4b0a9e9de924a2444cf82295baededcd3e
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-02-13 00:16:43 +0530

    yup shcema

--- React-Native-Projects/Project_3_PasswordGenerator/App.tsx
+++ React-Native-Projects/Project_3_PasswordGenerator/App.tsx
@@ -1,7 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+// Form valudation
+import * as Yup from 'yup'
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4, 'Should be minimum 4 characters')
+  .max(12, 'should be max of 16 characters')
+  .required('Length is required')
+})
 
 export default function App() {
+  
   return (
     <View>
       <Text>App</Text>
